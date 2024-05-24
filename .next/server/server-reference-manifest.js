83568f121ee3809dea37588daab3f47cb8dfa4c6
nlsmllr <info@nils-mueller.com>
2024-05-24 17:11:57 +0200

    feat category filter

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"AJOIrDifHYmx4pNXNCd2sOC1mNsxWwuxVGFiasOibr0=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"hyhJJBcWk8erUJX9ykDF8hmFc48uN6/fKUjiWMLvheU=\"\n}"
--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a1a777876964a4fe"; }
+/******/ 		__webpack_require__.h = function() { return "29910d94da7626d4"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
