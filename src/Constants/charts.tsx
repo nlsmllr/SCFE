8a4cc43ac0d84ae2d9c9e31f5cf5241dadb1a3ed
nlsmllr <info@nils-mueller.com>
2024-06-28 14:41:16 +0200

    add: API fetch

--- src/Constants/charts.tsx
+++ src/Constants/charts.tsx
@@ -6,15 +6,20 @@ import { BarChrt } from '../app/Components/Atoms/BarChrt';
 import { PieChrt } from '../app/Components/Atoms/PieChrt';
 import { Categories } from './categories';
 import tempforecast from './data/tempforecast.json';
+import { useState, useEffect } from 'react';
+
+// URLs of the API endpoint
+const temperatureURL = 'http://localhost:8080/api/weather/temperature'
 
 
+  
 // Displayed charts in order
 export const charts = [
   { component: <PieChrt title='Air' subtitle='(%)' data={tempforecast} />, categories: [Categories.Weather, Categories.Traffic], colSpan: 1 },
   { component: <MultiLineChrt title='Air Pollution' subtitle='(%)' data={tempforecast} />, categories: [Categories.Weather], colSpan: 2 },
   { component: <SingleLineChrt title='Humidity' subtitle='(%)' data={tempforecast} />, categories: [Categories.Weather], colSpan: 1 },
   { component: <RadarChrt title='Radar' subtitle='(%)' data={tempforecast} />, categories: [Categories.Weather], colSpan: 1 },
-  { component: <BarChrt title='Temperature' subtitle='(°C)' data={tempforecast} />, categories: [Categories.Weather], colSpan: 1 },
+  { component: <BarChrt title='Temperature' subtitle='(°C)' URL= {temperatureURL} />, categories: [Categories.Weather], colSpan: 1 },
   { component: <Map title='Hamburg' subtitle='today' />, categories: [Categories.Traffic], colSpan: 3 },
   { component: <MultiLineChrt title='Air Pollution' subtitle='(%)' data={tempforecast} />, categories: [Categories.Weather], colSpan: 1 },
   { component: <MultiLineChrt title='Co2 Concentration' subtitle='(%)' data={tempforecast} />, categories: [Categories.Parking], colSpan: 2 },
@@ -22,9 +27,9 @@ export const charts = [
   { component: <MultiLineChrt title='Wind Speed' subtitle='(KM/H)' data={tempforecast} />, categories: [Categories.Traffic], colSpan: 1 },
   { component: <SingleLineChrt title='Humidity' subtitle='(%)' data={tempforecast} />, categories: [Categories.Weather], colSpan: 1 },
   { component: <MultiLineChrt title='Wind Speed' subtitle='(KM/H)' data={tempforecast} />, categories: [Categories.Traffic], colSpan: 2 },
-  { component: <BarChrt title='Temperature' subtitle='(°C)' data={tempforecast} />, categories: [Categories.Weather], colSpan: 2 },
+  { component: <BarChrt title='Temperature' subtitle='(°C)'  URL= {temperatureURL}/>, categories: [Categories.Weather], colSpan: 2 },
   { component: <RadarChrt title='Radar' subtitle='(%)' data={tempforecast} />, categories: [Categories.Weather], colSpan: 1 },
   { component: <Map title='Hamburg' subtitle='today' />, categories: [Categories.Traffic], colSpan: 3 },
   { component: <MultiLineChrt title='CO2 Concentration' subtitle='(%)' data={tempforecast} />, categories: [Categories.Parking], colSpan: 2 },
-  { component: <iframe src="https://www.wetteronline.de/wetter/wedel" title="W3Schools Free Online Web Tutorials" className='w-full h-[800px]'></iframe>, categories: [Categories.Parking], colSpan: 3 },
+  // { component: <iframe src="https://www.wetteronline.de/wetter/wedel" title="W3Schools Free Online Web Tutorials" className='w-full h-[800px]'></iframe>, categories: [Categories.Parking], colSpan: 3 },
 ];
