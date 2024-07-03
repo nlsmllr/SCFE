905af947034ca47235344cb6162a454b2a54b6cf
nlsmllr <info@nils-mueller.com>
2024-07-03 16:28:23 +0200

    added: Webcam UNFINISHED

--- src/app/Components/Atoms/Webcam.tsx
+++ src/app/Components/Atoms/Webcam.tsx
@@ -1,19 +1,15 @@
-// app/Components/Atoms/WebcamStream.js
-
 import React from 'react';
 
-const WebcamStream = ({ title, url }: { title: string, url: string }) => {
+interface WebcamStreamProps {
+  streamUrl: string;
+}
+
+const WebcamStream: React.FC<WebcamStreamProps> = ({streamUrl }) => {
   return (
-    <div className="webcam-stream">
-      <h3>{title}</h3>
-      <iframe
-        src={url}
-        frameBorder="0"
-        allowFullScreen
-        title={title}
-        width="100%"
-        height="450"
-      ></iframe>
+    <div className="webcamBox">
+      <div className="videoWrapper" style={{ position: 'relative', paddingBottom: '56.25%', height: 0, overflow: 'hidden' }}>
+        <video data-html5-video="" muted={true} poster="images/live.jpg?t=1720016381842" preload="metadata" src="blob:https://cam.grassau.com/c2c8822d-6873-4e18-9edf-9737f888d455"></video>
+      </div>
     </div>
   );
 };
